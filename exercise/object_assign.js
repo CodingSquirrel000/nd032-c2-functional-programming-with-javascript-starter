ab937858308f3ce344eeb7bc8dd32a6b3110386a
CodingSquirrel000 <yang.jieq@northeastern.edu>
2022-04-04 10:02:19 -0400

    object_assign.js

---
+++ exercise/object_assign.js
@@ -0,0 +1,53 @@
+// Merge another character into this state. Keep at least one value and change at least one value
+
+let state = {
+    name: 'Wash',
+    ship: {
+        name: 'Serenity',
+        class: 'Firefly'
+    },
+    role: 'Pilot',
+    favoriteThing: {
+        item: "Toy",
+        details: {
+            type: 'Toy Tyrannosaurus Rex'
+        }
+    }
+}
+
+const incomingState = {
+    name: 'Mal',
+    role: 'Captain',
+    favoriteThing: {
+        item: "Not complicated"
+    },
+    history: ["Browncoat sergeant"]
+}
+
+state = Object.assign(state, incomingState);
+// Object.assign(target, source)
+
+// Resulting object
+// state = { 
+//     name: "Mal", 
+//     ship: { 
+//         name: "Serenity", 
+//         class: "Firefly" 
+//     }, 
+//     role: "Captain", 
+//     favoriteThing: { 
+//         item: "Not complicated" 
+//     }, 
+//     history: ["Browncoat sergeant"] 
+// } 
+
+// Your Code here
+const test = {
+    id: 123,
+    email: 'abc@123',
+    name: 'test',
+    favoriteThing: { item: 'debug' },
+}
+
+state = Object.assign(state, test);
+console.log(state);
